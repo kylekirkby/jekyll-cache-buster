da68fc109b5b82a3fd06e33736c8104442f2934b
Kyle Kirkby <kyle.kirkby@linaro.org>
2018-07-04 10:26:55 +0100

    Initial gem files

---
+++ jekyll-cache-buster/lib/jekyll/cache/buster/version.rb
@@ -0,0 +1,7 @@
+module Jekyll
+  module Cache
+    module Buster
+      VERSION = "0.1.0"
+    end
+  end
+end
---
+++ jekyll-cache-buster/spec/jekyll/cache/buster_spec.rb
@@ -0,0 +1,9 @@
+RSpec.describe Jekyll::Cache::Buster do
+  it "has a version number" do
+    expect(Jekyll::Cache::Buster::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
